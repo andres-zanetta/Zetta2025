d3ce16834d3e17a51f5890ce6a978eb17825f56b
AndresZanetta <andresnicolaszanetta@gmail.com>
2025-06-10 08:51:33 -0300

    Elimino Entidad Profesional (de mas) Se agrega primer Controller

---
+++ Zetta.Server/Controllers/PresupuestoControllers.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Zetta.Server.Controllers
+{
+    [ApiController]/*(alt91 alt93)*/
+    [Route("api/Presupuesto")]/*(alt91 alt93)*/
+    public class PresupuestoControllers:ControllerBase
+    {
+
+    }
+}
