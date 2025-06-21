f4d61bd5ffe16a13a3aa1a9b9bc3e41ef558505a
leonardo contreras <leo8292014@hotmail.com>
2025-06-21 19:02:45 -0300

    agregado de carpeta wwwroot con archivos para front

--- Zetta.Server/Program.cs
+++ Zetta.Server/Program.cs
@@ -1,15 +1,16 @@
-using Microsoft.EntityFrameworkCore;
-using Zetta.BD.DATA;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore; 
+using Zetta.BD.DATA; 
 
-#region Configuracion Servicios Ctor de la app
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 builder.Services.AddDbContext<Context>(op =>
     op.UseSqlServer("name=conn"));
@@ -20,19 +21,15 @@ builder.Services.AddCors(options =>
     options.AddPolicy("PermitirOrigenWeb",
         policy =>
         {
-            policy.WithOrigins("http://localhost:5500", "http://127.0.0.1:5500") // Agrega aquí la URL donde tu frontend se ejecutará
+            policy.WithOrigins("http://localhost:5500", "http://127.0.0.1:5500") 
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
 });
 
-#endregion
-
-#region Construccion de la app
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -40,12 +37,15 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-app.UseCors("PermitirOrigenWeb");
-
+app.UseCors("PermitirOrigenWeb"); // Si tienes CORS configurado
 app.UseAuthorization();
 
-app.MapControllers();
+
+app.UseStaticFiles(); // Habilita servir archivos de wwwroot
+app.UseRouting();
+app.MapControllers(); 
+
+
+app.MapFallbackToFile("index.html");
 
 app.Run();
-#endregion
