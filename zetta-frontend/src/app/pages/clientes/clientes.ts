3607b47f788bca582cebada054f13810572d2ca5
leonardo829 <leo8292014@hotmail.com>
2025-08-18 09:47:43 -0300

    cambios front clientes

--- zetta-frontend/src/app/pages/clientes/clientes.ts
+++ zetta-frontend/src/app/pages/clientes/clientes.ts
@@ -6,24 +6,20 @@ import { ClienteService } from '../../services/cliente';
 import { Cliente } from '../../models/cliente.model';
 import { Router } from '@angular/router';
 
-// 1. Importa los módulos y los íconos necesarios para Font Awesome
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-clientes',
   standalone: true,
-  // 2. Agrega FontAwesomeModule al array de imports
   imports: [CommonModule, FontAwesomeModule],
   templateUrl: './clientes.html',
   styleUrl: './clientes.css'
 })
 export class Clientes implements OnInit {
   clientes: Cliente[] = [];
-  // Se conserva el elemento `cargando`
   cargando = true;
 
-  // 3. Define los íconos como propiedades de la clase
   faEdit = faEdit;
   faTrash = faTrash;
 
