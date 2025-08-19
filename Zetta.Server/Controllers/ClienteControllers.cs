eeddafa0972239f8ce2a2688651b6aa65f1c8543
leonardo contreras <leo8292014@hotmail.com>
2025-08-19 00:49:09 -0300

    prueba 2

--- Zetta.Server/Controllers/ClienteControllers.cs
+++ Zetta.Server/Controllers/ClienteControllers.cs
@@ -36,7 +36,7 @@ namespace Zetta.Server.Controllers
 
             if (cliente == null)
             {
-                return NotFound("Cliente no Encontrado.");
+                return NotFound("Cliente no encontrado.");
             }
                
 
