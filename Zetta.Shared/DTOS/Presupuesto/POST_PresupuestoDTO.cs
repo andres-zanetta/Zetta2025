0931fd7bfd9687bbda89a6b69292af91aca09fc6
AndresZanetta <andresnicolaszanetta@gmail.com>
2025-09-06 01:46:02 -0300

    Propiedades del DTO

--- Zetta.Shared/DTOS/Presupuesto/POST_PresupuestoDTO.cs
+++ Zetta.Shared/DTOS/Presupuesto/POST_PresupuestoDTO.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Zetta.Shared.DTOS.ItemPresupuesto;
 
 namespace Zetta.Shared.DTOS.Presupuesto
 {
     public class POST_PresupuestoDTO
     {
+        public string ClienteNombre { get; set; }
+        public string Direccion { get; set; }
+        public string Localidad { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
 
+        public bool IncluyeMateriales { get; set; }
+
+        // Items que componen el presupuesto
+        public required List<GET_ItemPresupuestoDTO> Items { get; set; }
     }
+
 }
